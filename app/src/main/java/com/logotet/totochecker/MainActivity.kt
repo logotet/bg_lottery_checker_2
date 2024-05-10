9b1b52d405b7f36a7678823c8f83b9eb6ff020d2
v.vasilev <v.vasilev@up2technology.com>
2024-05-10 10:06:37 +0300

    feat(composable): Ball With number

--- app/src/main/java/com/logotet/totochecker/MainActivity.kt
+++ app/src/main/java/com/logotet/totochecker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.logotet.totochecker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,11 +11,38 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.coroutineScope
+import com.logotet.totochecker.data.remote.RemoteWinningNumbersDataSource
+import com.logotet.totochecker.presentation.ui.main.MainScreen
 import com.logotet.totochecker.ui.theme.TotoCheckerTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var remoteWinningNumbersDataSource: RemoteWinningNumbersDataSource
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        lifecycle.coroutineScope.launch {
+            remoteWinningNumbersDataSource.getWinningNumbers49().forEach{
+                Log.e("blia", "6 49 $it", )
+            }
+            remoteWinningNumbersDataSource.getWinningNumbers42().forEach{
+                Log.d("blia", "6 42 $it", )
+            }
+            remoteWinningNumbersDataSource.getWinningNumbers35FirstPick().forEach{
+                Log.e("blia", "5 35 (1) $it", )
+            }
+            remoteWinningNumbersDataSource.getWinningNumbers35SecondPick().forEach{
+                Log.d("blia", "5 35 (2) $it", )
+            }
+        }
+
         setContent {
             TotoCheckerTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +50,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
---
+++ app/src/main/java/com/logotet/totochecker/presentation/ui/composables/Ball.kt
@@ -0,0 +1,47 @@
+package com.logotet.totochecker.presentation.ui.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.logotet.totochecker.R
+
+@Composable
+fun Ball(
+    ballValue: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .size(60.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.image_white_ball),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+
+        Text(
+            text = ballValue,
+            fontSize = 18.sp,
+            style = TextStyle(
+                fontWeight = FontWeight.Bold
+            ), modifier = Modifier
+                .align(Alignment.Center)
+                .padding(16.dp)
+        )
+    }
+}
