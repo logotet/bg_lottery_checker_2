1d1b927bd182d61113345b1e3b16ddb1a733853d
v.vasilev <v.vasilev@up2technology.com>
2024-05-15 13:30:10 +0300

    refactor: Home roue title

--- app/src/main/java/com/logotet/totochecker/presentation/navigation/Route.kt
+++ app/src/main/java/com/logotet/totochecker/presentation/navigation/Route.kt
@@ -1,6 +1,6 @@
 package com.logotet.totochecker.presentation.navigation
 
-sealed class Route(var route: String, var title: String) {
+sealed class Route(var route: String, var label: String) {
     data object Home : Route(HOME_ROUTE, HOME_TITLE)
     data object Check : Route(CHECK_ROUTE, CHECK_TITLE)
     data object MyNumbers : Route(MY_NUMBERS_ROUTE, MY_NUMBERS_TITLE)
@@ -10,7 +10,7 @@ sealed class Route(var route: String, var title: String) {
         private const val CHECK_ROUTE: String = "check"
         private const val MY_NUMBERS_ROUTE: String = "local"
 
-        private const val HOME_TITLE: String = "home"
+        private const val HOME_TITLE: String = "Home"
         private const val CHECK_TITLE: String = "Check"
         private const val MY_NUMBERS_TITLE: String = "My numbers"
     }
