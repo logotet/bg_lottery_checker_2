67d1703f2369a5736bbf0fd7c7e8ec6651d77f92
v.vasilev <v.vasilev@up2technology.com>
2024-05-15 14:03:16 +0300

    feat: Bottom bar app navigation

--- app/src/main/java/com/logotet/totochecker/MainActivity.kt
+++ app/src/main/java/com/logotet/totochecker/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.logotet.totochecker.presentation.navigation.Route
+import com.logotet.totochecker.presentation.ui.composables.BottomAppBar
 import com.logotet.totochecker.presentation.ui.main.MainScreen
 import com.logotet.totochecker.ui.theme.TotoCheckerTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,32 +24,40 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             TotoCheckerTheme {
+                val navController = rememberNavController()
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen()
+                    Scaffold(
+                        bottomBar = {
+                            BottomAppBar(navController = navController)
+                        }
+                    ) { paddingValues ->
+                        NavHost(
+                            modifier = Modifier.padding(paddingValues),
+                            navController = navController,
+                            startDestination = Route.Home.route
+                        ) {
+                            composable(Route.Home.route) {
+                                MainScreen()
+                            }
+
+                            composable(Route.Check.route) {
+                                MainScreen(backgroundColor = Color.Gray)
+                            }
+
+                            composable(Route.MyNumbers.route) {
+                                MainScreen(backgroundColor = Color.Yellow)
+                            }
+                        }
+                    }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TotoCheckerTheme {
-        Greeting("Android")
     }
 }
