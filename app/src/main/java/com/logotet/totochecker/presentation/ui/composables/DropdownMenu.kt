587ee13c7ec8b45c5360a69c9b39511604a91fef
v.vasilev <v.vasilev@up2technology.com>
2024-05-23 15:33:10 +0300

    feat: Drop down menu

---
+++ app/src/main/java/com/logotet/totochecker/presentation/ui/composables/DropdownMenu.kt
@@ -0,0 +1,71 @@
+package com.logotet.totochecker.presentation.ui.composables
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.logotet.totochecker.domain.data.LotteryType
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropdownMenu(
+    modifier: Modifier = Modifier,
+    options: List<LotteryType>,
+    onValueChange: (LotteryType) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    var selectedValue by remember { mutableStateOf(options[0]) }
+
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+        modifier = modifier.padding(vertical = 20.dp)
+    ) {
+        OutlinedTextField(
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth(),
+            readOnly = true,
+            value = selectedValue.typeName,
+            onValueChange = {},
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            },
+            shape = RoundedCornerShape(corner = CornerSize(10.dp)),
+            colors = OutlinedTextFieldDefaults.colors()
+        )
+
+        ExposedDropdownMenu(
+            modifier = Modifier.fillMaxWidth(),
+            expanded = expanded,
+            onDismissRequest = { expanded = false }) {
+            options.forEach { option ->
+                DropdownMenuItem(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = { Text(text = option.typeName) },
+                    onClick = {
+                        expanded = false
+                        selectedValue = option
+                        onValueChange(selectedValue)
+                    }
+                )
+            }
+        }
+    }
+}
