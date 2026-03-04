2d4a1024d9faccca819f6c4ea940ccb392ea9793
v.vasilev <v.vasilev@up2technology.com>
2026-03-04 12:52:17 +0200

    feat: Set ktor client

---
+++ app/src/main/java/com/logotet/totochecker/data/remote/KtorClient.kt
@@ -0,0 +1,50 @@
+package com.logotet.totochecker.data.remote
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+
+class  KtorClient(
+    val httpClient: HttpClient
+) {
+    suspend fun get(
+        path: String = EMPTY_PATH,
+        vararg parameters: Pair<String, String>
+    ): HttpResponse {
+        val url = BASE_URL.plus(path)
+
+        return httpClient.get(url) {
+            parameters.forEach { (key, value) ->
+                parameter(key, value)
+            }
+        }
+    }
+
+    suspend inline fun <reified T> post(
+        path: String = EMPTY_PATH,
+        vararg parameters: Pair<String, String>,
+        body: T
+    ): HttpResponse {
+        val url = BASE_URL.plus(path)
+
+        return httpClient.post(url) {
+            parameters.forEach { (key, value) ->
+                parameter(key, value)
+            }
+
+            contentType(ContentType.Application.Json)
+
+            setBody(body)
+        }
+    }
+
+    companion object {
+        const val BASE_URL = "https://totofastapi2.vercel.app/"
+        const val EMPTY_PATH = ""
+    }
+}
--- app/src/main/java/com/logotet/totochecker/domain/di/NetworkModule.kt
+++ app/src/main/java/com/logotet/totochecker/domain/di/NetworkModule.kt
@@ -1,26 +1,70 @@
 package com.logotet.totochecker.domain.di
 
-import com.logotet.totochecker.data.remote.JsoupClient
+import android.util.Log
+import com.logotet.totochecker.data.remote.KtorClient
 import com.logotet.totochecker.data.remote.RemoteWinningNumbersDataSource
 import com.logotet.totochecker.domain.data.WinningNumbersDataSource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import javax.inject.Singleton
+
+private const val HTTP_LOGGER_TAG = "KTOR"
+private const val TIMEOUT_MILLIS = 30000L
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
     @Singleton
     @Provides
-    fun provideJsoupClient(): JsoupClient =
-        JsoupClient()
+    fun provideWinningNumbersDataSource(
+        ktorClient: KtorClient
+    ): WinningNumbersDataSource =
+        RemoteWinningNumbersDataSource(ktorClient)
 
     @Singleton
     @Provides
-    fun provideWinningNumbersDataSource(
-        jsoupClient: JsoupClient
-    ): WinningNumbersDataSource =
-        RemoteWinningNumbersDataSource(jsoupClient)
+    fun provideHttpClient(): HttpClient =
+        HttpClient(OkHttp) {
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        ignoreUnknownKeys = true
+                        prettyPrint = true
+                    }
+                )
+            }
+            install(Logging) {
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Log.i(HTTP_LOGGER_TAG, message )
+                    }
+                }
+                level = LogLevel.ALL
+            }
+            install(HttpTimeout) {
+                connectTimeoutMillis = TIMEOUT_MILLIS
+                socketTimeoutMillis = TIMEOUT_MILLIS
+                requestTimeoutMillis = TIMEOUT_MILLIS
+            }
+
+            expectSuccess = true
+        }
+
+    @Singleton
+    @Provides
+    fun provideKtorClient(
+        httpClient: HttpClient
+    ): KtorClient =
+        KtorClient(httpClient)
 }
